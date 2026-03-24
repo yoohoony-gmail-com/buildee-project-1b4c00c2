07263ce3a75ee4a275afeaf48590fc1d7cbde8fc
yoohoony-gmail-com <yoohoony@gmail.com>
2026-03-24 23:18:51 +0900

    Add structure/src/config/api.ts

---
+++ structure/src/config/api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: '/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+apiClient.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error('API Error:', error.response?.data || error.message)
+    return Promise.reject(error)
+  }
+)
+
+export default apiClient
